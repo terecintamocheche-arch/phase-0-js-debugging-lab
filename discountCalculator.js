0f2abaa7690c4072a281bc95c2bcc425fa69c653
Terecinta Mocheche <terecintamocheche@gmail.com>
2026-09-24 23:12:01 +0300

    Debugging the code

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,22 +1,4 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
-    let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
-        totalPrice += pricePerItem;
-    }
-
-    if (quantity >= 10) {
-        totalPrice *= 0.9;
-    }
-
-    return totalPrice;
-}
-
-
-
-module.exports = calculateDiscountedPrice;
-
-function calculateDiscountedPrice(quantity, pricePerItem) {
     debugger;
 
     let totalPrice = 0;
